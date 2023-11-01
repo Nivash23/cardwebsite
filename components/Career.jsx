5bcf3d17a12edea557789de423ba1c44dfaff5a9
Nivash23 <nivashsubash32@gmail.com>
2023-11-02 00:58:02 +0530

    updating2

--- components/Career.jsx
+++ components/Career.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import Card from '../components/Card'
+import Card from './Card'
+import career from './images/career.jpeg'
 
 function Career() {
   return (
     <div className='container-fluid d-flex justify-content-start'>
       <div className="row">
         <div className="col-md-3">
-          <Card  title='Career' image='../career.jpeg'/>
+          <Card title='Career' image={career } />
         </div>
         <div className="col-md-3">
-          <Card title='Career' image='../career1.jpeg'/>
+          <Card title='Career' image='../components/images/career1.jpeg'/>
         </div>
         <div className="col-md-3">
-          <Card title='Career' image='../career2.png' />
+          <Card title='Career' image='../components/images/career2.png' />
         </div>
         <div className="col-md-3">
-          <Card title='Career' image='../career3.jpeg' />
+          <Card title='Career' image='../components/images/career3.jpeg' />
         </div>
       </div>
       
