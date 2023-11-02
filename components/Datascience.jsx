f18b606ff0d84ca00a1abbf4157b1e1418a65306
Nivash23 <nivashsubash32@gmail.com>
2023-11-02 06:15:48 +0530

    updating5

--- components/Datascience.jsx
+++ components/Datascience.jsx
@@ -9,16 +9,16 @@ function Datascience() {
   return (
     <div className='container-fluid d-flex justify-content-start'>
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='10 Best Data Science Frameworks in 2023' url='https://www.guvi.in/blog/10-best-data-science-frameworks/' description='Does data scientists fascinate you? If yes, you must definitely know about data science frameworks.' image={d} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='Future of Data Science and How You Can Thrive With It' url='https://www.guvi.in/blog/the-future-of-data-science/' description='‍ Data Science has emerged as a revolutionary field in the technology world, transforming the' image={d1} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='Extraordinary Data Science Projects for Beginners As Well as Veterans' url='https://www.guvi.in/blog/data-science-projects/' description='As the demand for data and the people that can conquer it, i.e. Data Scientists' image={d2} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='Best Data Science Books to Learn 2023' url='https://www.guvi.in/blog/data-science-books-to-learn/' description='Today, we’re going to talk about something really cool: data science. It’s all about using' image={d3} />
         </div>
       </div>
--- components/Fullstack.jsx
+++ components/Fullstack.jsx
@@ -10,16 +10,16 @@ function Fullstack() {
   return (
     <div className='container-fluid d-flex justify-content-start'>
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='The Ultimate Guide to Real-World Full-Stack Development Applications' url='https://www.guvi.in/blog/top-must-know-full-stack-development-applications/' description='Full-stack development has become increasingly popular in recent years, with companies seeking professionals who can' image={f} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='Top Skills To Become a Full-Stack Developer in 2023' url='https://www.guvi.in/blog/top-skills-to-become-a-full-stack-developer/' description='Are you interested in becoming a great full-stack developer? If so, then you’re at the' image={f1} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='Top 10 Full-Stack Developer Frameworks in 2023' url='https://www.guvi.in/blog/full-stack-developer-frameworks/' description='In the ever-evolving world of technology, the demand for full stack developers is at an' image={ f2} />
         </div>
-        <div className="col-md-3">
+        <div className="col-md-4">
           <Card title='5 Career Opportunities for Full Stack Development' url='https://www.guvi.in/blog/career-opportunities-for-full-stack-development/' description='Ever wondered what awesome job opportunities are waiting for you in the field of Full Stack Development? Well, this blog is just for you!' image={f3} />
         </div>
       </div>
